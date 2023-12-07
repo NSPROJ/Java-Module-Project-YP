427d002183f508de9570f04001cf5bff7e82105c
SS <dekgold@gmail.com>
2023-12-07 10:03:25 +0300

    Здравствуйте,Исправленный код.На счёт целочисленных значений не очень понял, но всё вроде как работает, а скопейками принимает через запятую.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,57 +10,6 @@ public class Main {
     }
 }
 
-class BillSplitter {
-    private List<String> items;
-    private double totalAmount;
-    private int numberOfPeople;
-
-    public BillSplitter() {
-        items = new ArrayList<>();
-        totalAmount = 0.0;
-        numberOfPeople = 0;
-    }
-
-    public void splitBill() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите количество персон для разделения счета: ");
-        numberOfPeople = scanner.nextInt();
-
-        while (true) {
-            System.out.print("Введите название товара или 'Завершить' для итога: ");
-            String itemName = scanner.next();
-
-            if (itemName.equalsIgnoreCase("завершить")) {
-                break;
-            }
-
-            System.out.print("Введите количество товара: ");
-            int quantity = scanner.nextInt();
-
-            System.out.print("Введите цену товара: ");
-            double price = scanner.nextDouble();
-
-            double itemTotal = quantity * price;
-            totalAmount += itemTotal;
-
-            items.add(itemName + " - " + String.format("%.2f", itemTotal) + " " + Rubles.getSingularOrPlural(itemTotal));
-        }
-    }
-
-    public void printBill() {
-        double amountPerPerson = totalAmount / numberOfPeople;
-
-        System.out.println("\nСчет:");
-        for (String item : items) {
-            System.out.println(item);
-        }
-
-        System.out.println("\nОбщая сумма: " + String.format("%.2f", totalAmount) + " " + Rubles.getSingularOrPlural(totalAmount));
-        System.out.println("Сумма на каждого: " + String.format("%.2f", amountPerPerson) + " " + Rubles.getSingularOrPlural(amountPerPerson) + " на человека");
-    }
-}
-
 class Rubles {
     public static String getSingularOrPlural(double amount) {
         int rubles = (int) amount;
